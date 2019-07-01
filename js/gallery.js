66e7ae4a29645ea73c6323faef8ad6b2d7a5b5f5
Jason <houchenglong@luojilab.com>
2019-07-01 13:56:04 +0800

    Site updated: 2019-07-01 13:56:04

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,12 @@ document.addEventListener('DOMContentLoaded', function () {
         $('.article').lightGallery({ selector: '.gallery-item' });
     }
     if (typeof ($.fn.justifiedGallery) === 'function') {
-        $('.justified-gallery').justifiedGallery();
+        $('.justified-gallery').justifiedGallery({
+            rowHeight: 200,
+            maxRowHeight: 16,
+            lastRow: 'nojustify',
+            margins: 3,
+            randomize: true
+        });
     }
 });
